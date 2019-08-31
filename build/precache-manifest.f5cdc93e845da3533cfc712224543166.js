f06d3e4190acc421e251684e0c47df6a0cf3bfeb
dinesh.kumar@wildjasmine.com-at-006378141167 <dinesh.kumar@wildjasmine.com>
2019-08-31 21:26:34 +0530

    build

---
+++ build/precache-manifest.f5cdc93e845da3533cfc712224543166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d121045cdba20adb4425fefd7f0e9e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d292cf4053950ec548e9",
+    "url": "/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "0e771e5dd2e6d2409d21",
+    "url": "/static/css/main.ab1f4dd3.chunk.css"
+  },
+  {
+    "revision": "d292cf4053950ec548e9",
+    "url": "/static/js/2.32b62bbc.chunk.js"
+  },
+  {
+    "revision": "0e771e5dd2e6d2409d21",
+    "url": "/static/js/main.1c9b6a81.chunk.js"
+  },
+  {
+    "revision": "a24bf203539b8ba64931",
+    "url": "/static/js/runtime~main.eba2ceb0.js"
+  }
+]);
